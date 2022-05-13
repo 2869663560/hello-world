40ccdd7859273573478764d67b63d478c5eed39f
2869663560 <105487976+2869663560@users.noreply.github.com>
2022-05-13 17:48:18 +0800

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+//c++头文件
+#include<iostream>
+//预编译处理指令
+using namespace std;
+//main函数
+int main() {
+	//c++输出helloworld并换行
+	cout << "helloworld" << endl;
+	//按任意键继续
+	system("pause");
+	return 0;
+}
